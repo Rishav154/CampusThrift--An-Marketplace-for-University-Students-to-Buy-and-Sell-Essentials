2d759e49ceafa313426570c5273fcc8435b19f52
Rishav154 <rishav15045@gmail.com>
2025-06-04 16:23:50 +0530

    Implemented Skeleton loading in ProductList component for improved user experience

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,6 +1,5 @@
 import ProductCard from "@/components/ProductCard.jsx";
 import {useSelector} from "react-redux";
-import {PuffLoader} from "react-spinners";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {
@@ -32,12 +31,21 @@ function ProductList({currentPage, setCurrentPage}) {
 
     if (isLoading) {
         return (
-            <div className="flex flex-col justify-center items-center h-96 space-y-4">
-                <PuffLoader size={60} color="#4F46E5"/>
-                <p className="text-lg text-gray-600 font-medium">Loading products...</p>
+            <div className="w-full px-4 sm:px-6 lg:px-8">
+                <div
+                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 lg:gap-12 my-6 sm:my-10">
+                    {Array.from({length: 8}).map((_, i) => (
+                        <div key={i} className="animate-pulse rounded-xl border p-4 shadow-sm space-y-4">
+                            <div className="h-40 bg-gray-200 rounded-lg"/>
+                            <div className="h-4 bg-gray-200 rounded w-3/4"/>
+                            <div className="h-4 bg-gray-200 rounded w-1/2"/>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
+
 
     return (
         <div className="w-full px-4 sm:px-6 lg:px-8">
